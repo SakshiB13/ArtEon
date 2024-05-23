fe9b9e5502ac04a18346e915abc8f1744c6826de
SakshiB13 <sakshibole013@gmail.com>
2024-05-23 14:54:54 +0530

    duration

--- src/components/Chatbox.jsx
+++ src/components/Chatbox.jsx
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { collection, query, orderBy, getDocs, setDoc, doc, onSnapshot} from 'firebase/firestore';
+import { collection, query, orderBy, getDocs, setDoc, doc, onSnapshot } from 'firebase/firestore';
 import { db } from '../utils/firebase';
+import { useGlobalState, setGlobalState, setAlert } from '../store';
+import { buyNFT } from '../Blockchain.Services';
 
-
-const Chatbox = ({ auctionId, currentUser, onClose, auctionEndTime, auction }) => {
+const Chatbox = ({ auctionId, currentUser, onClose, auction }) => {
   const [bids, setBids] = useState([]);
   const [timer, setTimer] = useState(Math.floor((auction.endTime * 1000 - new Date().getTime()) / 1000));
   const [highestBid, setHighestBid] = useState(0);
+  const [highestBidderId, setHighestBidderId] = useState('');
   const [Bid, setBid] = useState(0);
+  const userName = useGlobalState('userName');
+  const userId = useGlobalState('userID');
 
   const [errorMessage, setErrorMessage] = useState('');
   const [showWinnerPopup, setShowWinnerPopup] = useState(false);
@@ -18,90 +22,100 @@ const Chatbox = ({ auctionId, currentUser, onClose, auctionEndTime, auction }) =
   const fetchBids = async () => {
     const bidsCollectionRef = collection(db, `auctions/${auctionid}/bids`);
     const bidsQuery = query(bidsCollectionRef, orderBy('amount', 'desc'));
-  
+
     try {
       const snapshot = await getDocs(bidsQuery);
       const bidsData = snapshot.docs.map(doc => {
         const bidData = doc.data();
         return {
           userId: bidData.userId,
+          userName: bidData.userName,
           amount: bidData.amount
         };
       });
       setBids(bidsData);
-  
+
       if (bidsData.length > 0) {
-        const highest = Math.max(...bidsData.map(bid => bid.amount));
-        setHighestBid(highest);
+        const highestBid = bidsData[0];
+        setHighestBid(highestBid.amount);
+        setHighestBidderId(highestBid.userId);
       }
     } catch (error) {
       console.error('Error fetching bids:', error);
     }
-  
+
     // Add listener for updates to bids
     const unsubscribe = onSnapshot(bidsQuery, (snapshot) => {
       const updatedBids = snapshot.docs.map(doc => {
         const bidData = doc.data();
         return {
           userId: bidData.userId,
+          userName: bidData.userName,
           amount: bidData.amount
         };
       });
       setBids(updatedBids);
-  
+
       if (updatedBids.length > 0) {
-        const highest = Math.max(...updatedBids.map(bid => bid.amount));
-        setHighestBid(highest);
+        const highestBid = updatedBids[0];
+        setHighestBid(highestBid.amount);
+        setHighestBidderId(highestBid.userId);
       }
     });
-  
+
     return unsubscribe; // Return the unsubscribe function
   };
-  
+
   useEffect(() => {
     const unsubscribe = fetchBids();
     // Return a cleanup function to unsubscribe when component unmounts
     return () => unsubscribe();
   }, []);
-   
-  
 
   useEffect(() => {
     const interval = setInterval(() => {
       setTimer(prevTimer => prevTimer - 1);
-      //fetchBids();
-      //console.log(bids);
     }, 1000);
-    
-    
 
     return () => clearInterval(interval);
   }, []);
 
-
   useEffect(() => {
-    fetchBids();
-      console.log(bids);
-  }, []);
-
-  useEffect(() => {
-    if (timer <= 0 && highestBid > 0) {
-      setShowWinnerPopup(true);
-    } else if (timer <= 0) {
-      handleClose();
-      setShowLoserMessage(true);
+    if (timer <= 0) {
+      if (highestBidderId === userId) {
+        setShowWinnerPopup(true);
+      } else {
+        setShowLoserMessage(true);
+      }
     }
-  }, [timer, highestBid]);
+  }, [timer, highestBid, highestBidderId, userId]);
 
   const handleClose = () => {
     setShowWinnerPopup(false);
     onClose();
   };
+
+  const winnerTransaction = async () => {
+    setGlobalState('showModal', 'scale-0');
+    setGlobalState('loading', {
+      show: true,
+      msg: 'Initializing NFT transfer...',
+    });
+
+    try {
+      await buyNFT({ tokenId: auction.tokenId, highestBid });
+      setAlert('Transfer completed...', 'green');
+      window.location.reload();
+    } catch (error) {
+      console.log('Error transferring NFT: ', error);
+      setAlert('Purchase failed...', 'red');
+    }
+  };
+
   const isNumeric = (value) => {
     return !isNaN(parseFloat(value)) && isFinite(value);
   };
 
-  
   const formatTime = (time) => {
     const hours = Math.floor(time / 3600);
     const minutes = Math.floor((time % 3600) / 60);
@@ -109,7 +123,6 @@ const Chatbox = ({ auctionId, currentUser, onClose, auctionEndTime, auction }) =
 
     return `${hours > 0 ? hours + "h " : ""}${minutes > 0 ? minutes + "m " : ""}${seconds}s`;
   };
-
 
   const handlePlaceBid = () => {
     const bidAmount = parseFloat(Bid);
@@ -128,12 +141,12 @@ const Chatbox = ({ auctionId, currentUser, onClose, auctionEndTime, auction }) =
 
     setDoc(newBidRef, {
       userId: currentUser,
+      userName: userName,
       amount: bidAmount,
       timestamp: new Date().getTime(),
     }).then(() => {
       setErrorMessage('');
       fetchBids(); // Fetch bids after successfully placing a bid
-      console.log(bids);
     }).catch(error => {
       console.error('Error placing bid:', error);
       setErrorMessage('Error placing bid');
@@ -144,43 +157,37 @@ const Chatbox = ({ auctionId, currentUser, onClose, auctionEndTime, auction }) =
     onClose(); // Call the onClose prop function to close the chatbox
   };
 
-
   return (
     <div className="chatbox-container">
       <div className="chatbox">
-      <div className="bids-list">
-  <h3>All Bids:</h3>
-  <div className="scrollable-text">
-    {bids.map((bid, index) => (
-      <span key={index}>{bid.userId}: {bid.amount}<br /></span>
-    ))}
-  </div>
-        <div className="input-container">
-          
-          <input
-            type="text"
-            placeholder="Place your bid..."
-            //value={highestBid}
-            onChange={(e) => setBid(e.target.value)}
-          />
-          <button onClick={handlePlaceBid}>Place Bid</button>
+        <div className="bids-list">
+          <h3>All Bids:</h3>
+          <div className="scrollable-text">
+            {bids.map((bid, index) => (
+              <span key={index}>{bid.userName}: {bid.amount}<br /></span>
+            ))}
+          </div>
+          <div className="input-container">
+            <input
+              type="text"
+              placeholder="Place your bid..."
+              onChange={(e) => setBid(e.target.value)}
+            />
+            <button onClick={handlePlaceBid}>Place Bid</button>
+          </div>
+
+          {errorMessage && <div className="error-message">{errorMessage}</div>}
+          <div className="info">
+            <div className="timer">Time Remaining: {formatTime(timer)}</div>
+            <div className="highest-bid">Current Highest Bid: {highestBid}</div>
+            <button className="close-button" onClick={handleCloseChatbox}>x</button>
+          </div>
         </div>
-
-        {errorMessage && <div className="error-message">{errorMessage}</div>}
-        <div className="info">
-        <div className="timer">Time Remaining: {formatTime(timer)}</div>
-          <div className="highest-bid">Current Highest Bid: {highestBid}</div>
-      <button className="close-button" onClick={handleCloseChatbox}>x</button>
-
-        </div>
-        
-</div>
-
       </div>
       {showWinnerPopup && (
         <div className="winner-popup">
           <div className="winner-message">Congratulations! You won the auction with the highest bid of {highestBid}.</div>
-          <button onClick={handleClose}>Confirm Transaction</button>
+          <button onClick={winnerTransaction}>Confirm Transaction</button>
         </div>
       )}
       {showLoserMessage && (
@@ -188,7 +195,6 @@ const Chatbox = ({ auctionId, currentUser, onClose, auctionEndTime, auction }) =
       )}
     </div>
   );
-
 };
 
 export default Chatbox;
